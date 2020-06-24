164fb7920a7ccd5ad16a86ef1815cc4de8227892
jordanrob709 <jordanrob709@gmail.com>
2020-06-24 18:07:42 +0300

    added user and store apps

--- zershecom/settings.py
+++ zershecom/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
+    'user.apps.UserConfig',
+    'store.apps.StoreConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
