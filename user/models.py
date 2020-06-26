7a40b67ad594ee95c87a0e5163f9b621c60f42b6
jordanrob709 <jordanrob709@gmail.com>
2020-06-26 11:48:40 +0300

    added customer & seller models

--- user/models.py
+++ user/models.py
@@ -6,3 +6,16 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Customer(CustomUser):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    address = models.CharField(max_length=180)
+
+
+class Seller(CustomUser):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    store_name = models.CharField(max_length=120)
+    address = models.CharField(max_length=180)
