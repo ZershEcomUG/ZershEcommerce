447815e4201266c88d0bfd14bb520f444baf8e3b
jordanrob709 <jordanrob709@gmail.com>
2020-06-26 12:27:29 +0300

    added category, product & order models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from user.models import Seller, Customer
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.FloatField()
+    sku = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
--- user/models.py
+++ user/models.py
@@ -13,9 +13,15 @@ class Customer(CustomUser):
     last_name = models.CharField(max_length=120)
     address = models.CharField(max_length=180)
 
+    def __str__(self):
+        return self.username
+
 
 class Seller(CustomUser):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     store_name = models.CharField(max_length=120)
     address = models.CharField(max_length=180)
+
+    def __str__(self):
+        return self.username
