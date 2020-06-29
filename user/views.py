66a25476870154e7da5f4b2c07bff3fcc8cd78ea
jordanrob709 <jordanrob709@gmail.com>
2020-06-29 18:19:25 +0300

    completed auth functionality

--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ from user.forms import CustomUserCreationForm
 # Create your views here.
 
 
-class SignupView(CreateView):
+class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
--- zershecom/urls.py
+++ zershecom/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('user.urls')),
     path('users/', include('django.contrib.auth.urls')),
+    path('', include('pages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
