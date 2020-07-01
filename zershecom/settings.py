32a060d99bf50dd60b42df6f1b6813a7cabe9aad
jordanrob709 <jordanrob709@gmail.com>
2020-07-01 18:12:42 +0300

    added modified login & signup with allauth & crispy_forms

--- zershecom/settings.py
+++ zershecom/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd party apps
     'crispy_forms',
+    'allauth',
+    'allauth.account',
 
 ]
 
@@ -130,12 +133,28 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'user.CustomUser'
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+#LOGIN_REDIRECT_URL = 'home'
+#LOGOUT_REDIRECT_URL = 'home'
 
 
 # media configurations
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# django-crispy-forms config
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# django-allauth config
+LOGIN_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT = 'home'
+
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+# email config
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
