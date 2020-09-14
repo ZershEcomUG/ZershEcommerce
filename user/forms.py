00ac9aed5edbc7573d3981d29dd1d0abf8fc7fec
jordanrob709 <jordanrob709@gmail.com>
2020-09-14 17:21:42 +0300

    added Seller signup form and view

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from django import forms
+from django.db import transaction
+from .models import CustomUser, Seller
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -14,3 +16,23 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = CustomUser
         fields = ('username', 'email')
+
+
+class SellerSignUpForm(UserCreationForm):
+    store_name = forms.CharField()
+    address = forms.CharField()
+    phone = forms.IntegerField()
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_seller = True
+        user.save()
+        seller = Seller.objects.create(user=user)
+        seller.store_name.add(*self.cleaned_data.get('store name'))
+        seller.address.add(*self.cleaned_data.get('address'))
+        seller.phone.add(*self.cleaned_data.get('phone'))
+        return user    
--- user/views.py
+++ user/views.py
@@ -1,4 +1,22 @@
-from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.shortcuts import redirect
 from django.views.generic import CreateView
-from user.forms import CustomUserCreationForm
+
+from .forms import SellerSignUpForm
+from .models import CustomUser
+
 # Create your views here.
+
+class SellerSignUpView(CreateView):
+    model = CustomUser
+    form_class = SellerSignUpForm
+    template_name = 'registration/seller_signup.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'student'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('students:quiz_list')
