e35241ffa1ea13c58a25e740baf429f95758ddb1
jordanrob709 <jordanrob709@gmail.com>
2020-09-24 03:03:17 +0300

    added production db settings

--- zershecom/settings.py
+++ zershecom/settings.py
@@ -151,7 +151,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 AUTH_USER_MODEL = 'user.CustomUser'
 
