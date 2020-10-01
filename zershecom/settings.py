6fa4dfd40877b45fd94adf19caa510090e9406e4
jordanrob709 <jordanrob709@gmail.com>
2020-10-01 14:18:49 +0300

    addjustments

--- zershecom/settings.py
+++ zershecom/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import os
 import dj_database_url
 import dotenv
 import django_heroku
-import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,9 +31,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eq)v7)#87b)+a_^ykuldxo0i7j+x0zpz)c7)iwgu+et7hc%=^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['dfb-zersh.herokuapp.com', 'zersh.shop', 'www.zersh.shop']
+ALLOWED_HOSTS = ['dfb-zersh.herokuapp.com', 'zersh.shop', 'www.zersh.shop', '*']
 
 
 # Application definition
